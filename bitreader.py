31cec127407053c55bf58b41c7e9a34d05d3b6ca
Chris Greene <archgoon@gmail.com>
2013-08-18 00:22:14 -0700

    Added bitreader

---
+++ bitreader.py
@@ -0,0 +1,75 @@
+import StringIO
+import math 
+
+class BitReader():
+    def __init__(self, byte_stream):
+        self.bytes = byte_stream
+        self.current_num = 0
+        self.available_bits = 0
+    def bytes_to_num(self, bytes):
+        l = len(bytes)
+        acc = 0
+        for byte in bytes:
+            acc += ord(byte)*256**(l-1)
+            l -= 1
+        return acc
+    def maybe_read_more(self, n):
+        if self.available_bits < n:
+            more_bytes = int(math.ceil((n-self.available_bits)/8.0))
+            new_bytes = self.bytes.read(more_bytes)
+            if new_bytes == '':
+                raise Exception("Insufficient bits")
+            self.current_num = 256**more_bytes*self.current_num
+            self.current_num += self.bytes_to_num(new_bytes)
+            self.available_bits += 8*more_bytes
+
+    def read_bits(self, n):
+        self.maybe_read_more(n)
+        ones_mask = 2**self.available_bits-1
+        low_bits = 2**(self.available_bits-n) - 1
+        high_bits = ones_mask - low_bits
+        nbits = self.current_num & high_bits
+        self.current_num -= high_bits
+        nbits = nbits >> (self.available_bits - n)
+        self.available_bits -= n
+        return nbits
+
+def testReadPastByte(byte):
+    bitReader = testReadByte(byte, [2,2,4])
+    bitReader = testReadByte(byte, [2,2,2,2])
+    try:
+        bitReader.read_bits(2)
+    except Exception as e:
+        print "Correctly detected Overread"
+
+def testReadByte(byte, groups):
+    bitReader = BitReader(StringIO.StringIO(byte))
+    bits_read = 0
+    for bits in groups:
+        bits_read += bits
+        print bitReader.read_bits(bits)
+    if bits_read != 8:
+        raise Exception("Entire byte not read: "+bits_read)
+    print "Read byte!"
+    return bitReader
+
+def testReadBytes(bytes, groups):
+    bitReader = BitReader(StringIO.StringIO(bytes))
+    bits_read = 0
+    for bits in groups:
+        bits_read += bits
+        print bitReader.read_bits(bits)
+    if bits_read != 8*len(bytes):
+        raise Exception("Entire bytes not read: %d/%d" 
+            % (bits_read, 8*len(bytes)))
+    print "Read byte!"
+
+def test():
+    testReadPastByte("\xff")
+    testReadBytes("\xff\xff", [2,8,6])
+    testReadBytes("\x0f\x0f", [2,8,6])
+    testReadBytes("\x00\x00", [2,8,6])
+    
+def main():
+    test()
+main()
--- h264.py
+++ h264.py
@@ -1,14 +1,7 @@
 from flvlib import tags
 from collections import defaultdict
-
-class BitReader():
-    def __init__(bytes):
-        self.bytes = bytes
-        self.offset_bits = 0
-        self.offset_bytes = 0
-    def bitmask(n):
-        return 256-1 - 2**(8-n)
-
+import sys
+   
 def bytes_to_num(bytes):
     l = len(bytes)
     acc = 0
@@ -18,27 +11,39 @@ def bytes_to_num(bytes):
     return acc
 
 def extract_frame_flv(flvtag, flvfile):
+    pos = flvfile.tell() # save position
     flvfile.seek(flvtag.offset)
     tagtype = flvfile.read(1)
     datasize = bytes_to_num(flvfile.read(3))
     timestamp = flvfile.read(3)
     timestampext = flvfile.read(1)
     streamid = flvfile.read(3)
-    data = flvfile.read(datasize)
+    data = flvfile.read(datasize) # h264 goodness! mmHHMM!
+    flvfile.seek(pos) # return to original position
     return data
 
 def h264_nalu(data):
     datasize = len(data)
     zero_bit = bytes_to_num(data[0]) & 128
-    nal_ref_idc = bytes_to_num(data[0]) & (127-31)
+    nal_ref_idc = (bytes_to_num(data[0]) & (127 & ~31)) >> 5
     nal_unit_type = bytes_to_num(data[0]) & 31
-    return zero_bit, nal_ref_idc, nal_unit_type
+    return zero_bit, nal_ref_idc, nal_unit_type, datasize
 
 def h264_read(flv, f):
     stats = defaultdict(int)
-    for i, tag in enumerate(flv.iter_tags()):
+    for i, tag in enumerate(flv.iter_tags(), 1):
         if isinstance(tag, tags.VideoTag):
-            result = h264_nalu(extract_frame_flv(tag, f))
+            zero_bit, nal_ref_idc, nal_unit_type, datasize = h264_nalu(extract_frame_flv(tag, f))
+            result = zero_bit, nal_ref_idc, nal_unit_type
+            print datasize
             stats[result] += 1
             print "Tag #%d" % i, result
     print stats
+
+def main(args):
+    print tags.strict_parser()
+    inputfile = open(args[0], 'rb')
+    flv = tags.FLV(inputfile)
+    h264_read(flv, inputfile)
+
+main(sys.argv[1:])
