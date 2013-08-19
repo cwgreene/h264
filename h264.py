60cee6e88085b07c963a2ac15fe5fd59590418ef
Chris Greene <archgoon@gmail.com>
2013-08-18 19:14:34 -0700

    Added sequence parameter sets and picture parameter sets logic
    need to add sgolomb. This is all basically from the spec

--- h264.py
+++ h264.py
@@ -15,6 +15,85 @@ def bytes_to_num(bytes):
         l -= 1
     return acc
 
+def read_sequence_paramter_set(data):
+    # TODO: Implement:
+    #        - sgolomb() in bitreader
+    #        - vui_paramaters()
+    #        - make these part of an actual object
+    bitdata = bitreader.BitReader(data)
+    profile_idc = bitdata.read_bits(8)
+    constraint_set0_flag = bitdata.read_bits(1)
+    constraint_set2_flag = bitdata.read_bits(1)
+    constraint_set2_flag = bitdata.read_bits(1)
+    reserved_zero_5bits = bitdata.read_bits(5)
+    level_idc = bitdata.read_bits(8)
+    seq_paramter_set_id = bitdata.read_ugolomb()
+    log2_max_frame_num_minus4 = bitdata.read_ugolomb()
+    pic_order_cnt_type = bitdata.read_ugolomb()
+    if pic_order_cnt_type == 0:
+        log2_max_pic_order_cnt_lsb_minus4 = bitdata.read_ugolomb()
+    elif (pic_order_cnt_type == 1):
+        delta_pic_order_always_zero_flag = bitdata.read_bits(1)
+        offset_for_non_ref_pic = bitdata.read_sgolomb()
+        offset_for_top_to_bottom_filed = bitdata.read_sgolomb()
+        num_ref_frames_in_pic_order_cnt_cycle = bitdata.read_ugolomb()
+        for i in range(num_ref_frames_in_pic_order_cnt_cycle):
+            offset_for_ref_frame.append(bit_data.read_sgolomb)
+    num_ref_frames = bit_data.read_ugolomb()
+    gaps_in_frame_num_value_allowed_flag = bit_data.read_bits(1)
+    pic_width_in_mbs_minus1 = bit_data.read_ugolomb()
+    pic_height_in_map_units_minus1 = bit_data.read_ugolomb()
+    frame_mbs_only_flag = bit_data.read_bits(1)
+    if not frame_mbs_only_flag:
+        mb_adapative_frame_field_flag = bit_data.read_bits(1)
+    direct_8x8_inference_flag = bit_data.read_bits(1)
+    frame_cropping_flag = bit_data.read_bits(1)
+    if frame_cropping_flag:
+        frame_crop_left_offst = bit_data.read_ugolomb()
+        frame_crop_right_offset = bit_data.read_ugolomb()
+        frame_crop_top_offset = bit_data.read_ugolomb()
+        frame_crop_bottom_offset = bit_data.read_ugolomb()
+    vui_paramaters_present_flag = bit_data.read_bits(1)
+    if vui_paramaters_present_flag:
+        vui_paramaters()
+
+def read_picture_paramter_set(data):
+    bitdata = bitreader.BitReader(data)
+    pic_parameter_set_id = bit_data.read_ugolomb()
+    seq_parameter_set_id = bit_data.read_ugolomb()
+    entropy_coding_mode_flag = bit_data.read_bits(1)
+    num_slice_groups_minus1 = bit_data.read_ugolomb()
+    if num_slice_groups_minus1 > 0:
+        slice_group_map_type = bit_data.read_ugolomb()
+        if slice_group_map_type == 0:
+            run_length_minus1 = []
+            for iGroup in range(num_slice_groups_minus1 + 1):
+                run_length_minus1.append(bit_data.read_ugolomb())
+        elif slice_group_map_type == 2:
+            top_left = []
+            bottom_right = []
+            for iGroup in range(num_slice_groups_minus1 + 1):
+                top_left.append(bit_data.read_ugolomb())
+                bottom_right.append(bit_data.read_ugolomb())
+        elif slice_group_map_type in [3,4,5]:
+            slice_group_change_direction_flag = bitdata.read_bits(1)
+            slice_gropu_change_rate_minus1 = bitdata.read_ugolomb()
+        elif slice_group_map_type == 6:
+            pic_size_in_map_units_minus1 = bitdata.read_ugolomb()
+            slice_group_id = []
+            for i in range(pic_size_in_map_units_minus1):
+                slice_group_id.append(bitdata.read_bits(1))
+    num_ref_idx_l0_active_minus1 = bitdata.read_ugolomb()
+    num_ref_idx_l1_active_minus1 = bitdata.read_ugolomb()
+    weighted_pred_flag = bitdata.read_bits(1)
+    weighted_bipred_idc = bitdata.read_bits(2)
+    pic_init_qp_minus26 = bitdata.read_sgolomb()
+    pic_init_qs_minus26 = bitdata.read_sgolomb()
+    chroma_qp_index_offset = bitdata.read_sgolomb()
+    deblocking_filter_control_present_flag = bitdata.read_bits(1)
+    redundant_pic_cnt_present_flag = bitdata.read_bits(1)
+    #rbsp trail
+            
 def read_tag_headers(flvtag, flvfile):
     pos = flvfile.tell() # save position
     flvfile.seek(flvtag.offset)
@@ -59,7 +138,20 @@ def h264_nalu(data):
     zero_bit = bitdata.read_bits(1)
     nal_ref_idc = bitdata.read_bits(2)
     nal_unit_type = bitdata.read_bits(5)
-    return zero_bit, nal_ref_idc, nal_unit_type, datasize
+    bytes_read = 1
+    rbsp_bytes = []
+    for i in range(1, datasize):
+        if i + 2 < datasize:
+            rbsp_bytes.append(bitreader.read(8))       
+            rbsp_bytes.append(bitreader.read(8))
+            third_byte = bitreader.read(8)
+            i += 2
+            if rbsp_bytes[-1] == 0 and rbsp_bytes[-2] == 0 and third_byte == 3:
+                continue
+            rbsp_bytes.append(third_byte)
+        elif:
+            rbsp_bytes.append(bitreader.read(8))
+    return zero_bit, nal_ref_idc, nal_unit_type, rbsp_bytes
 
 def readAVCDecoderConfigurationRecord(data):
     global lengthSizeMinusOne
